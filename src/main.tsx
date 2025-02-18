e5f40aef60f92d1516cbdcd686122102582fe7c0
CodedLibra <adeola.ogundijo@yahoo.com>
2025-02-18 22:36:36 +0100

    feat: configured QureryProvider for tanstack query and added BrowserRouter for routing and Toaster from react-hot toast

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import QueryProvider from "./providers/QueryProvider.tsx";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryProvider>
+      <BrowserRouter>
+        <Toaster position="top-center" reverseOrder={false} />
+        <App />
+      </BrowserRouter>
+    </QueryProvider>
   </StrictMode>
 );
