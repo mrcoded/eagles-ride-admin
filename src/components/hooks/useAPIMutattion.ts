62ef743b896843d46455d07b9b71fa88b5c9429c
CodedLibra <adeola.ogundijo@yahoo.com>
2025-04-01 22:39:00 +0100

    feat: created a useAPIMutation hook

---
+++ src/components/hooks/useAPIMutattion.ts
@@ -0,0 +1,54 @@
+import { useMutation } from "@tanstack/react-query";
+import { makeAPIRequest } from "@/lib/apiRequest";
+
+type LoginData = {
+  email: string;
+  password: string;
+};
+
+type UseAPIMutationOptions = {
+  endpoint: string;
+  method?: string;
+  onMutate?: () => void;
+  onSuccess?: (data: any) => void;
+  onError?: (error: Error) => void;
+};
+
+export function useAPIMutation(options: UseAPIMutationOptions) {
+  const { endpoint, method, onMutate, onSuccess, onError } = options;
+
+  return useMutation({
+    mutationFn: async (data: LoginData) => {
+      try {
+        const response = await makeAPIRequest({
+          endpoint,
+          data,
+          method,
+        });
+
+        // Example check for invalid credentials (customize as needed)
+        if (response.message === "Invalid Credentials") {
+          throw new Error(response.message);
+        }
+        return response;
+      } catch (error: unknown) {
+        throw new Error((error as Error).message);
+      }
+    },
+    onMutate: () => {
+      if (onMutate) {
+        onMutate();
+      }
+    },
+    onSuccess: (data) => {
+      if (onSuccess) {
+        onSuccess(data);
+      }
+    },
+    onError: (error: any) => {
+      if (onError) {
+        onError(error);
+      }
+    },
+  });
+}
