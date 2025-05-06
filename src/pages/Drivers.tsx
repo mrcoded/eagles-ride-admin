eba5d79fdf55f84bc662c681606241b27ec59f20
CodedLibra <adeola.ogundijo@yahoo.com>
2025-05-06 23:21:50 +0100

    refactor: added apiMutation function, loading state, removed unused type definition and passed mutation and isLoading as props to UserModal

--- src/pages/Drivers.tsx
+++ src/pages/Drivers.tsx
@@ -3,37 +3,22 @@ import { useQuery } from "@tanstack/react-query";
 
 import UserModal from "@/components/modals/UserModal";
 import RideBookingsTable from "@/components/tables/RideBookingsTable";
-
-interface RideBookingsTableProps {
-  _id: string;
-  fullname: string;
-  email: string;
-  isDriverApproved: boolean;
-  status: string;
-}
-[];
-
-interface BookingsModalProps {
-  users: {
-    _id: string;
-    fullname: string;
-    email: string;
-    phone_number: string;
-    address: string;
-  }[];
-}
+import { BookingsModalProps, BookingsTableProps } from "@/types";
+import { useAPIMutation } from "@/components/hooks/useAPIMutation";
+import toast from "react-hot-toast";
 
 function Drivers() {
+  const [isLoading, setIsLoading] = useState(false);
   const [userModalOpen, setUserModalOpen] = useState(false);
   const [selectedUserId, setSelectedUserId] = useState<string | undefined>("");
-  const [selectedRideId, setSelectedRideId] = useState<string | null>(null);
+  const [selectedItemId, setSelectedItemId] = useState<string | null>(null);
 
   const {
     status,
     data: driversData,
     error,
     isFetching,
-  } = useQuery<RideBookingsTableProps>({
+  } = useQuery<BookingsTableProps["drivers"]>({
     queryKey: ["drivers"],
   });
 
@@ -46,44 +31,64 @@ function Drivers() {
     queryKey: ["users"],
   });
 
+  // Use the useAPImutation hook for logging in
+  const mutation = useAPIMutation({
+    endpoint: `admin/approve-driver/${selectedItemId}`,
+    method: "PATCH",
+    onMutate: () => {
+      console.log("Approving Driver request...");
+      setIsLoading(true);
+    },
+    onError: (error) => {
+      setIsLoading(false);
+      toast.error(error.message);
+      console.log(error);
+    },
+  });
+
   const { users } = userData ?? { user: undefined };
   console.log(users, driversData);
   return (
-    <div className="flex-1">
+    <div className="flex-1 ">
       {/* Main Content */}
       <main className="flex flex-col md:flex-row gap-2 px-2 pt-2">
         {/* Left Section */}
-        <section className="flex-1">
+        <section className="w-full flex-1">
           {/* Header */}
           <header className="bg-white shadow py-1 px-6">
             <h1 className="text-xl font-bold">Rent Buddy</h1>
           </header>
           {/* Upcoming Bookings */}
           <h2 className="text-base font-semibold mb-2">Upcoming Bookings</h2>
+          {/* <div className="flex flex-col flex-1"> */}
           <RideBookingsTable
             drivers={driversData}
             // bookings={rides}
-            selectedRideId={selectedRideId}
-            setSelectedRideId={setSelectedRideId}
+            selectedItemId={selectedItemId}
+            setSelectedItemId={setSelectedItemId}
             setSelectedUserId={setSelectedUserId}
             setUserModalOpen={setUserModalOpen}
           />
-          <header className="bg-white shadow py-2.5 px-6">
-            <h1 className="text-xl font-bold">Pagination</h1>
-          </header>
+          <div className="bg-white shadow py-2.5 px-6 mt-auto">
+            <div className="text-xl font-bold">Pagination</div>
+          </div>
+          {/* </div> */}
         </section>
 
         {/* Right Section */}
-        <section className="w-full md:w-[30%] flex flex-col gap-1">
-          {userModalOpen && (
+        {userModalOpen && (
+          <section className="w-full md:w-[30%] flex flex-col gap-1">
             <UserModal
+              mutation={mutation}
+              isLoading={isLoading}
+              setIsLoading={setIsLoading}
               userId={selectedUserId}
-              selectedRideId={selectedRideId}
-              rideData={driversData}
+              dataSource={driversData}
               userFetching={userFetching}
+              selectedItemId={selectedItemId}
             />
-          )}
-        </section>
+          </section>
+        )}
       </main>
     </div>
   );
