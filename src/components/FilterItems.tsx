0795e3acc8303e0773350a1879081a102e4ab8e6
CodedLibra <adeola.ogundijo@yahoo.com>
2025-05-11 23:41:33 +0100

    feat: created a filter item component

---
+++ src/components/FilterItems.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import React from "react";
+import { cn } from "@/lib/utils";
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandGroup,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+const filters = [
+  {
+    value: "booked",
+    label: "Booked",
+  },
+  {
+    value: "ongoing",
+    label: "Ongoing",
+  },
+  {
+    value: "assigned",
+    label: "Assigned",
+  },
+];
+
+export function FilterItems({
+  query,
+  setQuery,
+  title,
+}: {
+  title: string;
+  query: string;
+  setQuery: React.Dispatch<React.SetStateAction<string>>;
+}) {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between h-7"
+        >
+          {query
+            ? filters.find((filter) => filter.value === query)?.label
+            : `Filter ${title}...`}
+          <ChevronsUpDown className="opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandList>
+            <CommandGroup>
+              {filters.map((filter) => (
+                <CommandItem
+                  key={filter.value}
+                  value={filter.value}
+                  onSelect={(currentValue) => {
+                    setQuery(currentValue === query ? "" : currentValue);
+                    setOpen(false);
+                  }}
+                  className="cursor-pointer hover:font-medium"
+                >
+                  {filter.label}
+                  <Check
+                    className={cn(
+                      "ml-auto",
+                      query === filter.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
