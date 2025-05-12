6fbc2869d40a152008d7ced8215f5d8d195fa406
CodedLibra <adeola.ogundijo@yahoo.com>
2025-05-12 23:14:59 +0100

    chore: renamed hooks

--- src/components/hooks/useAPIMutation.ts
+++ src/components/hooks/useAPIMutation.ts
@@ -1,5 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
 import { makeAPIRequest } from "@/lib/apiRequest";
+import { FieldValue, FieldValues } from "react-hook-form";
 
 type LoginData = {
   email: string;
@@ -18,20 +19,21 @@ export function useAPIMutation(options: UseAPIMutationOptions) {
   const { endpoint, method, onMutate, onSuccess, onError } = options;
 
   return useMutation({
-    mutationFn: async (data: LoginData) => {
+    mutationFn: async (data: FieldValues) => {
       try {
         const response = await makeAPIRequest({
           endpoint,
           data,
           method,
         });
-
+        console.log(response);
         // Example check for invalid credentials (customize as needed)
         if (response.message === "Invalid Credentials") {
           throw new Error(response.message);
         }
         return response;
       } catch (error: unknown) {
+        console.log(error);
         throw new Error((error as Error).message);
       }
     },
