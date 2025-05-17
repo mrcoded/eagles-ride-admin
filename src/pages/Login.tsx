1a38df77a90816a14b1a35629e9b34b0f77ca9aa
CodedLibra <adeola.ogundijo@yahoo.com>
2025-05-17 21:29:02 +0100

    feat: built a Login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/components/forms/LoginForm";
+
+function Login() {
+  return (
+    <div className="grid min-h-screen place-items-center">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default Login;
