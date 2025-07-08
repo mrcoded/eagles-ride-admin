2185237488a3da3adb06176ce5042ec69cc58c59
CodedLibra <adeola.ogundijo@yahoo.com>
2025-07-09 00:00:04 +0100

    added filterItems constant files

---
+++ src/constants/filterItems.ts
@@ -0,0 +1,14 @@
+export const filters = [
+  {
+    value: "booked",
+    label: "Booked",
+  },
+  {
+    value: "ongoing",
+    label: "Ongoing",
+  },
+  {
+    value: "assigned",
+    label: "Assigned",
+  },
+];
