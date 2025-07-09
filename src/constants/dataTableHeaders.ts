91356b9cd51dd4d1d90a5129626f08570da3285a
CodedLibra <adeola.ogundijo@yahoo.com>
2025-07-09 17:56:13 +0100

    added dataTableHeaders constant files

---
+++ src/constants/dataTableHeaders.ts
@@ -0,0 +1,21 @@
+export const DRIVERS_TABLE_HEADERS = [
+  "",
+  "Name",
+  "Email",
+  "Phone",
+  "Approval",
+  "Driver Status",
+  "Redundant",
+  "",
+];
+
+export const RIDES_TABLE_HEADERS = [
+  "",
+  "Name",
+  "Status",
+  "Trip Type",
+  "Start Date",
+  "Pickup",
+  "Dropoff",
+  "",
+];
