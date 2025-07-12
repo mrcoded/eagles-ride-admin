fe5b98fa2c99811df4e55cc54081c09cc7dd88f9
CodedLibra <adeola.ogundijo@yahoo.com>
2025-07-12 23:59:09 +0100

    feat: created an UnassignDriver component

---
+++ src/components/UnassignDriver.tsx
@@ -0,0 +1,67 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { AssignDriverProps } from "@/types";
+import useUnAssignDriver from "@/hooks/useUnassignDriver";
+import { useGlobalContext } from "@/context/GlobalContext";
+
+function UnAssignDriver({
+  setIsLoading,
+  selectedItemId,
+  selectedRideData,
+}: AssignDriverProps) {
+  //get driver id from global context
+  const { driverId, isOpen, setIsOpen } = useGlobalContext();
+
+  //unassign driver to ride
+  const unAssignDriverHandler = useUnAssignDriver({
+    selectedItemId,
+    selectedRideData,
+    setIsLoading,
+  });
+
+  return (
+    <>
+      {isOpen && (
+        <Dialog>
+          <DialogTrigger className="text-red-500 underline font-medium">
+            Unassign
+          </DialogTrigger>
+          <DialogContent className="w-1/3">
+            <DialogHeader>
+              <DialogTitle>Confirm Unassign</DialogTitle>
+              <DialogDescription className="flex justify-between">
+                <span>Are you sure you want to unassign driver?</span>
+              </DialogDescription>
+            </DialogHeader>
+            <div className="inline-flex justify-between">
+              <button
+                onClick={(e) => {
+                  setIsOpen(false);
+                  unAssignDriverHandler?.(e, driverId);
+                }}
+                className="bg-green-500 text-slate-200 rounded-sm py-1.5 px-2 text-sm"
+              >
+                Confirm
+              </button>
+              <button
+                onClick={() => setIsOpen(false)}
+                className="bg-red-500 text-slate-200 rounded-sm py-1.5 px-2 text-sm"
+              >
+                Cancel
+              </button>
+            </div>
+          </DialogContent>
+        </Dialog>
+      )}
+    </>
+  );
+}
+
+export default UnAssignDriver;
