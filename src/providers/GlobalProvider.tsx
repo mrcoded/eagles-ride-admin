79a494d9a091d19b823ba2e8cedb0753fb7a6c11
CodedLibra <adeola.ogundijo@yahoo.com>
2025-07-23 00:00:37 +0100

    feat: added states to GlobalContext Provider

--- src/providers/GlobalProvider.tsx
+++ src/providers/GlobalProvider.tsx
@@ -6,12 +6,32 @@ interface GlobalProviderProps {
 }
 
 const GlobalProvider: React.FC<GlobalProviderProps> = ({ children }) => {
+  const [query, setQuery] = useState("");
   const [isOpen, setIsOpen] = useState(true);
   const [driverId, setDriverId] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedUserId, setSelectedUserId] = useState<string | undefined>("");
+  const [selectedItemId, setSelectedItemId] = useState<string | null>(null);
 
   return (
     <GlobalContext.Provider
-      value={{ driverId, setDriverId, isOpen, setIsOpen }}
+      value={{
+        query,
+        setQuery,
+        isLoading,
+        setIsLoading,
+        isModalOpen,
+        setIsModalOpen,
+        selectedUserId,
+        setSelectedUserId,
+        selectedItemId,
+        setSelectedItemId,
+        driverId,
+        setDriverId,
+        isOpen,
+        setIsOpen,
+      }}
     >
       {children}
     </GlobalContext.Provider>
