902ab3da97dc5aafecef4a26870904f115af5cca
CodedLibra <adeola.ogundijo@yahoo.com>
2025-07-23 00:03:56 +0100

    feat: added driverFilters constant files

--- src/constants/filterItems.ts
+++ src/constants/filterItems.ts
@@ -1,4 +1,4 @@
-export const filters = [
+export const bookingFilters = [
   {
     value: "booked",
     label: "Booked",
@@ -10,5 +10,20 @@ export const filters = [
   {
     value: "assigned",
     label: "Assigned",
+  },
+  {
+    value: "none",
+    label: "Remove Filter",
+  },
+];
+
+export const driverFilters = [
+  {
+    value: "unassigned",
+    label: "UnAssigned",
+  },
+  {
+    value: "none",
+    label: "Remove Filter",
   },
 ];
