1e29806bf393fd4cf79d22d0efdbbda4318180f6
CodedLibra <adeola.ogundijo@yahoo.com>
2025-08-14 23:15:59 +0100

    feat: created a header component

---
+++ src/components/shared/Header.tsx
@@ -0,0 +1,35 @@
+import SearchInput from "../SearchInput";
+import { FilterItems } from "../FilterItems";
+
+import { Menu, Settings, UserCircle2 } from "lucide-react";
+import { useGlobalContext } from "@/context/GlobalContext";
+
+function Header() {
+  const { sidebarOpen, setSidebarOpen, toolbarTitle } = useGlobalContext();
+
+  return (
+    <header className="sticky top-0 z-40 flex h-12 lg:h-16 items-center justify-between px-4 lg:px-6 gap-2 bg-white border-b space-x-5">
+      {!sidebarOpen && (
+        <Menu
+          onClick={() => setSidebarOpen(!sidebarOpen)}
+          className="flex lg:hidden items-center text-slate-800 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-gray-700 hover:font-semibold hover:text-primary rounded-md transition-colors"
+        />
+      )}
+
+      {/* toolbar */}
+      <div className="flex justify-between items-center gap-2 flex-grow">
+        <SearchInput title={toolbarTitle} />
+        <div className="text-slate-200 text-[9px] font-medium">
+          <FilterItems title={toolbarTitle} />
+        </div>
+      </div>
+
+      <div className="flex items-center justify-end gap-2.5">
+        <Settings className="size-6" />
+        <UserCircle2 className="size-6" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
