75148f888c8be3cd21b68438d38402a491e2fa3b
CodedLibra <adeola.ogundijo@yahoo.com>
2025-09-08 00:49:20 +0100

    feat: added logout function from authcontext to logout button on sidebar

--- src/components/shared/SidebarNavItems.tsx
+++ src/components/shared/SidebarNavItems.tsx
@@ -1,13 +1,19 @@
 import { Link, useLocation } from "react-router-dom";
+import { cn } from "@/lib/utils";
 import { LogOut } from "lucide-react";
 
-import { cn } from "@/lib/utils";
-import { navigationItems } from "@/constants/sidebarNavLinks";
+import { Button } from "../ui/button";
+
 import { ToggleTheme } from "../ToggleTheme";
+import { useAuthContext } from "@/hooks/useAuthContext";
+import { navigationItems } from "@/constants/sidebarNavLinks";
 
 function SidebarNavItems() {
   const urlPath = useLocation();
   const { pathname } = urlPath;
+
+  //logout handler
+  const { logout } = useAuthContext();
 
   return (
     <>
@@ -48,10 +54,14 @@ function SidebarNavItems() {
       <div className="flex items-center w-full mt-14 p-4">
         <ul className="space-y-4">
           <ToggleTheme />
-          <li className="flex items-center py-2 px-3 text-slate-800 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-gray-700 hover:font-semibold hover:text-primary rounded-md transition-colors cursor-pointer">
+          <Button
+            variant="link"
+            onClick={() => logout()}
+            className="flex items-center py-2 px-3 text-slate-800 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-gray-700 hover:font-semibold hover:text-primary rounded-md transition-colors cursor-pointer"
+          >
             <LogOut className="size-3.5" />
             <span className="ml-3 font-medium text-sm">Logout</span>
-          </li>
+          </Button>
         </ul>
       </div>
     </>
