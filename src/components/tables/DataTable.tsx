52af1d0e89ef3d8e517024ae7e217b13d726a731
CodedLibra <adeola.ogundijo@yahoo.com>
2025-11-01 23:49:57 +0100

    refactor

--- src/components/tables/DataTable.tsx
+++ src/components/tables/DataTable.tsx
@@ -1,87 +1,71 @@
-"use client";
-
+import Pagination from "../Pagination";
 import TableRowItem from "./TableRowItem";
+import { TableLoader } from "../TableLoader";
 
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import { DataTableProps, TableType } from "./types";
 
-import { BookingsTableProps } from "@/types";
+import { useGlobalContext } from "@/hooks/useGlobalContext";
+
 import {
   DRIVERS_TABLE_HEADERS,
   RIDES_TABLE_HEADERS,
 } from "@/constants/dataTableHeaders";
-import { useGlobalContext } from "@/context/GlobalContext";
 
-const DataTable = ({ data, type, isLoading }: BookingsTableProps) => {
+const DataTable = <T extends TableType>({
+  data,
+  type,
+  isLoading,
+}: DataTableProps<T>) => {
+  const { currentPage, selectedItemId } = useGlobalContext();
+
+  // Pagination
+  const PAGESIZE = 13;
+  const startIndex = (currentPage - 1) * PAGESIZE;
+  const endIndex = startIndex + PAGESIZE;
+  const totalPages = Math.ceil(data.length / PAGESIZE);
+
   // GET Table headers
   const TABLE_HEADERS =
-    type === "booking"
-      ? RIDES_TABLE_HEADERS
-      : type === "driver"
-      ? DRIVERS_TABLE_HEADERS
-      : [];
+    type === "booking" ? RIDES_TABLE_HEADERS : DRIVERS_TABLE_HEADERS;
 
-  const {
-    selectedItemId,
-    setSelectedUserId,
-    setSelectedItemId,
-    setIsModalOpen,
-  } = useGlobalContext();
-
-  // Handle checkbox change
-  const handleCheckboxChange = (
-    checked: boolean,
-    userId: string,
-    itemId: string
-  ) => {
-    if (checked) {
-      setSelectedItemId(itemId);
-      setSelectedUserId(userId);
-      setIsModalOpen(true);
-    } else {
-      setSelectedItemId(null);
-      setSelectedUserId("");
-      setIsModalOpen(false);
-    }
-  };
+  // Get paginated data
+  const paginatedData = data?.slice(startIndex, endIndex);
 
   return (
-    <table className="w-full bg-white dark:bg-slate-800 rounded shadow overflow-hidden">
-      <thead className="bg-orange-400 dark:bg-orange-600 text-slate-800 dark:text-slate-100 text-xs uppercase">
-        <tr>
-          {TABLE_HEADERS.map((header, index) => (
-            <th key={index} className="p-1.5 text-left whitespace-nowrap">
-              {header}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {isLoading
-          ? Array(10)
-              .fill(null)
-              .map((_, i) => (
-                <tr key={i} className="h-8 border-b">
-                  {Array(8)
-                    .fill(null)
-                    .map((_, idx) => (
-                      <td key={idx} className="p-1.5">
-                        <Skeleton height={15} />
-                      </td>
-                    ))}
-                </tr>
-              ))
-          : data?.map((item) => (
+    <>
+      <table className="w-full bg-white dark:bg-slate-800 rounded shadow overflow-hidden">
+        <thead className="bg-orange-400 dark:bg-orange-600 text-slate-800 dark:text-slate-100 text-xs uppercase">
+          <tr>
+            {TABLE_HEADERS.map((header, index) => (
+              <th key={index} className="p-1.5 text-left whitespace-nowrap">
+                {header}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {isLoading ? (
+            <TableLoader />
+          ) : (
+            paginatedData?.map((item) => (
               <TableRowItem
                 key={item._id}
                 item={item}
                 type={type}
                 selectedItemId={selectedItemId}
-                onSelect={handleCheckboxChange}
               />
-            ))}
-      </tbody>
-    </table>
+            ))
+          )}
+        </tbody>
+      </table>
+
+      {/* Pagination */}
+      {data.length >= 13 && (
+        <div className="flex bg-white px-6">
+          <Pagination totalPages={totalPages} />
+        </div>
+      )}
+    </>
   );
 };
 
