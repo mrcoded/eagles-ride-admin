cb2db2c922313b5e0bc12ad4dfa9d8d3a66a2c11
CodedLibra <adeola.ogundijo@yahoo.com>
2025-12-10 15:20:05 +0100

    feat: created a SelectedItemDetails component

---
+++ src/components/SelectedItemDetails.tsx
@@ -0,0 +1,15 @@
+import DriverInfoModal from "@/components/modals/DriverInfoModal";
+import BookedRideInfo from "@/components/bookings/BookedRideInfo";
+
+function SelectedItemDetails({ isDriver }: { isDriver?: boolean | undefined }) {
+  return (
+    <div className="flex flex-col justify-start items-start w-full px-3 gap-2">
+      <h3 className="flex items-center gap-2 p-1.5 text-sm font-medium text-slate-200 capitalize tracking-wide bg-orange-500 dark:bg-orange-600 rounded-sm w-full">
+        {isDriver ? "Driver's Uploaded Information" : "Booked Ride Information"}
+      </h3>
+      {isDriver ? <DriverInfoModal /> : <BookedRideInfo />}
+    </div>
+  );
+}
+
+export default SelectedItemDetails;
