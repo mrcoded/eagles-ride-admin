76274ebc876df9ac62ec9fc441c77dec8e327445
CodedLibra <adeola.ogundijo@yahoo.com>
2025-12-12 18:09:32 +0100

    configure queryClient

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -1,9 +1,20 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
-
 export default function QueryProvider({ children }: { children: ReactNode }) {
+  // provide the default query function to your app with defaultOptions
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: 1,
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
+
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
