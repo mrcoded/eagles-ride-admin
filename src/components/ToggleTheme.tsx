9a9b2ae0361605d58aae28d02e4cc89a98055991
CodedLibra <adeola.ogundijo@yahoo.com>
2025-12-22 13:53:23 +0100

    fix: added event.stopPropagation function to toggleTheme to prevent event bubling to parent onClick on mobile and used a button component instead of li element

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { LucideMoon, LucideSun } from "lucide-react";
 
 import { useTheme } from "@/providers/ThemeProvider";
+import { Button } from "@/components/ui/button";
 
 export function ToggleTheme() {
   const { setTheme } = useTheme();
@@ -9,13 +10,16 @@ export function ToggleTheme() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   // Toggle theme function
-  const toggleTheme = () => {
+  const toggleTheme = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation();
+
     setIsDarkMode(!isDarkMode);
     setTheme(isDarkMode ? "light" : "dark");
   };
 
   return (
-    <li
+    <Button
+      variant="link"
       onClick={toggleTheme}
       className="flex items-center py-2 px-3 text-slate-50 lg:text-slate-800 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-gray-700 hover:font-semibold hover:text-primary rounded-md transition-colors cursor-pointer"
     >
@@ -27,6 +31,6 @@ export function ToggleTheme() {
       <span className="ml-3 font-medium text-sm">
         {isDarkMode ? "Light Mode" : "Dark Mode"}
       </span>
-    </li>
+    </Button>
   );
 }
