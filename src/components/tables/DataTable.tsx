84d579a0172fa6703ce78530d09879b522f59278
CodedLibra <adeola.ogundijo@yahoo.com>
2025-12-31 17:34:54 +0100

    feat: added dataError as props and wrapped my table in it incase of error

--- src/components/tables/DataTable.tsx
+++ src/components/tables/DataTable.tsx
@@ -7,14 +7,15 @@ import { DataTableProps, TableType } from "../../types/tables";
 import { useGlobalContext } from "@/hooks/useGlobalContext";
 
 import {
-  DRIVERS_TABLE_HEADERS,
   RIDES_TABLE_HEADERS,
+  DRIVERS_TABLE_HEADERS,
 } from "@/constants/dataTableHeaders";
 
 const DataTable = <T extends TableType>({
   data,
   type,
   isLoading,
+  dataError,
 }: DataTableProps<T>) => {
   const { currentPage, selectedItemId } = useGlobalContext();
 
@@ -33,32 +34,42 @@ const DataTable = <T extends TableType>({
 
   return (
     <>
-      <table className="w-full bg-white dark:bg-slate-800 rounded shadow overflow-hidden">
-        <thead className="bg-orange-400 dark:bg-orange-600 text-slate-800 dark:text-slate-100 text-xs uppercase">
-          <tr>
-            {TABLE_HEADERS.map((header, index) => (
-              <th key={index} className="p-1.5 text-left whitespace-nowrap">
-                {header}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {isLoading ? (
-            <TableLoader />
-          ) : (
-            paginatedData?.map((item) => (
-              <TableRowItem
-                key={item._id}
-                item={item}
-                type={type}
-                selectedItemId={selectedItemId}
-              />
-            ))
-          )}
-        </tbody>
-      </table>
-
+      {/* Display error message if dataError exists */}
+      {dataError ? (
+        <div className="flex items-center justify-center text-red-500 text-2xl">
+          Oops! Something went wrong
+        </div>
+      ) : data?.length === 0 ? (
+        <div className="flex items-center justify-center text-2xl">
+          Search Data Not Found!
+        </div>
+      ) : (
+        <table className="w-full rounded shadow overflow-hidden">
+          <thead className="bg-orange-400 dark:bg-slate-600 text-slate-800 dark:text-slate-200 text-xs uppercase">
+            <tr>
+              {TABLE_HEADERS.map((header, index) => (
+                <th key={index} className="p-1.5 text-center whitespace-nowrap">
+                  {header}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {isLoading ? (
+              <TableLoader />
+            ) : (
+              paginatedData?.map((item) => (
+                <TableRowItem
+                  key={item._id}
+                  item={item}
+                  type={type}
+                  selectedItemId={selectedItemId}
+                />
+              ))
+            )}
+          </tbody>
+        </table>
+      )}
       {/* Pagination */}
       {data.length >= 13 && (
         <div className="flex bg-white px-6">
